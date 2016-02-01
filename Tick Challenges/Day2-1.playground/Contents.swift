165cbd2d189907adc4e4ab96dc3124d4506d9221
Liam Byrne <student@MacBook-Pro-Liam-Byrne.local>
2016-02-01 10:34:55 -0500

    Finished Challenge 1
    Completed

--- Tick Challenges/Day2-1.playground/Contents.swift
+++ Tick Challenges/Day2-1.playground/Contents.swift
@@ -2,4 +2,22 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+print("How many antennas?")
+var antennas = 2
+print("How many eyes?")
+var eyes = 3
+
+    
+//TroyMartian, who has at least 3 antenna and at most 4 eyes.
+//VladSaturnian, who has at most 6 antenna and at least 2 eyes.
+//GraemeMercurian, who has at most 2 antenna and at most 3 eyes.
+
+if antennas >= 3 && eyes >= 4 {
+    print("TroyMartian")
+}
+if antennas <= 6 && eyes >= 2 {
+    print("VladSaturnian")
+}
+if antennas <= 2 && eyes <= 3 {
+    print("GreameMercurian")
+}
