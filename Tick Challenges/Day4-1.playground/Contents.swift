ec98a8e448d7c0698e9c30d5421daecaa635e76e
Liam Byrne <student@MacBook-Pro-Liam-Byrne.local>
2016-02-11 10:10:19 -0500

    Finished challenge 4-1
    4-1 completed

--- Tick Challenges/Day4-1.playground/Contents.swift
+++ Tick Challenges/Day4-1.playground/Contents.swift
@@ -3,3 +3,20 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+var speed = 100
+var speedLimit = 100
+var fine = 0
+
+if (speed <= speedLimit) {
+    print("Congratulations, you are within the speed limit!")
+} else if (speed > speedLimit) && (speed <= (speedLimit + 20)) {
+    fine = 100
+    print("You are speeding and your fine is $\(fine)!")
+} else if (speed >= (speedLimit + 21)) && (speed <= (speedLimit + 30)) {
+    fine = 270
+    print("You are speeding and your fine is $\(fine)!")
+} else if (speed >= 31) {
+    fine = 500
+    print("You are speeding and your fine is $\(fine)!")
+}
