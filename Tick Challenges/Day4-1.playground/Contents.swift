057a4ac981fe7774dbe745b130c8d81f3129f31b
Liam Byrne <student@MacBook-Pro-Liam-Byrne.local>
2016-02-11 10:22:54 -0500

    Finished challenge 4-2
    Completed 4-2

--- Tick Challenges/Day4-1.playground/Contents.swift
+++ Tick Challenges/Day4-1.playground/Contents.swift
@@ -4,19 +4,19 @@ import Cocoa
 
 var str = "Hello, playground"
 
-var speed = 100
+var speed = 300
 var speedLimit = 100
 var fine = 0
 
-if (speed <= speedLimit) {
+if speed <= speedLimit {
     print("Congratulations, you are within the speed limit!")
-} else if (speed > speedLimit) && (speed <= (speedLimit + 20)) {
+} else if speed > speedLimit && speed <= (speedLimit + 20) {
     fine = 100
     print("You are speeding and your fine is $\(fine)!")
-} else if (speed >= (speedLimit + 21)) && (speed <= (speedLimit + 30)) {
+} else if speed >= (speedLimit + 21) && speed <= (speedLimit + 30) {
     fine = 270
     print("You are speeding and your fine is $\(fine)!")
-} else if (speed >= 31) {
+} else if speed >= 31 {
     fine = 500
     print("You are speeding and your fine is $\(fine)!")
 }
--- Tick Challenges/Day4-2.playground/Contents.swift
+++ Tick Challenges/Day4-2.playground/Contents.swift
@@ -2,4 +2,15 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+var fishReading1 = 5
+var fishReading2 = 2
+var fishReading3 = 1
+var fishReading4 = 0
+
+if fishReading1 < fishReading2 && fishReading2 < fishReading3 && fishReading3 < fishReading4 {
+    print("Fish Rising")
+} else if fishReading1 > fishReading2 && fishReading2 > fishReading3 && fishReading3 > fishReading4 {
+    print("Fish Diving")
+} else {
+    print("No Fish")
+}
